2acd8be27a283af2a38076c7413e442ba4df7a4b
daridev <darideveloper@gmail.com>
2025-10-02 14:09:01 -0600

    fix (next.config.mjs): allow aws images

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,30 @@
-import mdx from '@next/mdx';
-import createNextIntlPlugin from 'next-intl/plugin';
+import mdx from '@next/mdx'
+import createNextIntlPlugin from 'next-intl/plugin'
 
 const withMDX = mdx({
-    extension: /\.mdx?$/,
-    options: { },
-});
+  extension: /\.mdx?$/,
+  options: {},
+})
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
-    images: {
-        formats: ['image/webp'],
-        minimumCacheTTL: 300, // 5 minutes cache
-    },
-};
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  images: {
+    formats: ['image/webp'],
+    minimumCacheTTL: 300, // 5 minutes cache
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ]
+  },
+}
 
-export default withNextIntl(withMDX(nextConfig));
+export default withNextIntl(withMDX(nextConfig))
