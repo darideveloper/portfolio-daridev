9e99986dfc2276a6352b0bcde14a90ec5c5fd1f1
daridev <darideveloper@gmail.com>
2025-10-28 15:59:58 -0600

    feat (/blog/page.tsx): render all posts with images

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -98,8 +98,7 @@ export default function Blog(
             </Heading>
 			<Flex
 				fillWidth flex={1} direction="column">
-				<Posts range={[1,3]} locale={locale} thumbnail/>
-				<Posts range={[4]} columns="2" locale={locale}/>
+				<Posts range={[1]} locale={locale} thumbnail/>
 			</Flex>
             {contact.display && (
                 <ContactForm display={contact.display} />
